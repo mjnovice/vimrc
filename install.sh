7483076ee08f0e06fc01e0e89afba53bd510010e
Mayank Jha <mayank.jha@thecarousell.com>
2019-07-25 16:36:48 +0800

    go bins

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/.vimrc
 vim +PluginInstall +qall
+vim +GoInstallBinaries
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
