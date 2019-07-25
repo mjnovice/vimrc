737c7fd799ea690dd2cff3f91a575f0b8fede8f7
Mayank Jha <mayank.jha@thecarousell.com>
2019-07-25 15:01:06 +0800

    install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp .vimrc ~/.vimrc
+vim +PluginInstall +qall
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
