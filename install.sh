6e8ae578ae989ab7fe42bb3f94b51f2d1b892b48
Mayank Jha <mayank25080562@gmail.com>
2019-09-07 15:26:39 -0400

    installing zsh as linux

--- install.sh
+++ install.sh
@@ -3,4 +3,8 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/.vimrc
 vim +PluginInstall +qall
 vim +GoInstallBinaries
+#assuming it is for debian based linux machines
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
